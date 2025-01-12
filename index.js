e501610ddbf8d1574c2fec4c4fc8cdf23c6f260b
marindeveloper <randomcatz12@outlook.com>
2025-01-12 22:50:44 +0100

    first commit yaaaayayayayay

---
+++ index.js
@@ -0,0 +1,12 @@
+/* const move = document.getElementById("MouseGlow");
+
+document.body.onpointermove = event => {
+    const { clientX, clientY } = event;
+
+    move.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    
+    }, {duration: 1000, fill: "forwards"})
+
+} */
